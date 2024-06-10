fa60bf445f989dea439b8e57d2efeca21632a606
İlyas Özkurt <ilyasozkurt@gmail.com>
2024-06-10 03:04:49 +0300

    Sound support demo implemented.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,37 @@
 <template>
 
   <div class="wheel">
+    <div class="wheel-state">
+      <div v-if="winnerResult">
+        Winner: <span :style="{'color': winnerResult.color}">{{ winnerResult.text }}</span> 🎉
+      </div>
+      <div v-else-if="isSpinning">
+        Spinning...
+      </div>
+      <div v-else>
+        Ready to spin?
+      </div>
+    </div>
     <VueWheelSpinner
         ref="spinner"
         :slices="slices"
-        :winnerIndex="winnerIndex"
-        :spinDuration="spinDuration"
-        :spinButtonLabel="spinButtonLabel"
-        :spinButtonBackgroundColor="spinButtonBackgroundColor"
-        :spinButtonLabelColor="spinButtonLabelColor"
+        :winner-index="winnerIndex"
+        :spin-duration="spinDuration"
+        :spin-button-label="spinButtonLabel"
+        :spin-button-size="spinButtonSize"
+        :spin-button-background-color="spinButtonBackgroundColor"
+        :spin-button-label-color="spinButtonLabelColor"
+        :needle-background-color="needleBackgroundColor"
+        :needle-scale="needleScale"
+        :is-spinning="isSpinning"
+        :sounds="sounds"
         @spin-start="onSpinStart"
         @spin-end="onSpinEnd"
     />
   </div>
 
   <div>
-    <button v-for="(slice, index) in slices" :disabled="spinning" @click="spinFor(index)">
+    <button v-for="(slice, index) in slices" :disabled="isSpinning" @click="spinFor(index)">
       Win for {{ slice.text }}
     </button>
   </div>
@@ -25,26 +41,43 @@
 <script>
 import VueWheelSpinner from 'vue-wheel-spinner';
 
+import wonSound from './sounds/won.mp3';
+import clickSound from './sounds/click.mp3';
+import hoverSound from './sounds/hover.mp3';
+import leaveSound from './sounds/leave.mp3';
+import spinningSound from './sounds/spinning.mp3';
+
 export default {
   components: {
     VueWheelSpinner
   },
   data() {
     return {
+      winnerResult: null,
       slices: [
-        { color: '#FF0000', text: 'Prize 1' },
-        { color: '#00FF00', text: 'Prize 2' },
-        { color: '#0000FF', text: 'Prize 3' },
-        { color: '#FFFF00', text: 'Prize 4' },
-        { color: '#FFA500', text: 'Prize 5' },
-        { color: '#800080', text: 'Prize 6' }
+        {color: '#FF0000', text: 'Prize 1'},
+        {color: '#00FF00', text: 'Prize 2'},
+        {color: '#0000FF', text: 'Prize 3'},
+        {color: '#FFFF00', text: 'Prize 4'},
+        {color: '#FFA500', text: 'Prize 5'},
+        {color: '#800080', text: 'Prize 6'},
       ],
       winnerIndex: 1,
       spinDuration: 4000,
-      spinning: false,
+      isSpinning: false,
       spinButtonLabel: 'Spin',
-      spinButtonBackgroundColor: '#eaeaea',
-      spinButtonLabelColor: '#000'
+      spinButtonSize: 20,
+      spinButtonBackgroundColor: '#fff',
+      spinButtonLabelColor: '#000',
+      needleBackgroundColor: '#fff',
+      needleScale: 1.5,
+      sounds: {
+        won: wonSound,
+        spinButtonClick: clickSound,
+        spinButtonHover: hoverSound,
+        spinButtonLeave: leaveSound,
+        spinning: spinningSound
+      }
     };
   },
   methods: {
@@ -53,11 +86,12 @@ export default {
       this.$refs.spinner.spinWheel(index);
     },
     onSpinStart() {
-      this.spinning = true;
+      this.winnerResult = null;
+      this.isSpinning = true;
     },
     onSpinEnd(winnerIndex) {
-      this.spinning = false;
-      alert(`Winner: ${this.slices[winnerIndex].text}`);
+      this.isSpinning = false;
+      this.winnerResult = this.slices[winnerIndex];
     }
   }
 };
@@ -66,10 +100,18 @@ export default {
 <style scoped>
 
 .wheel {
-  display: flex;
-  justify-content: center;
-  align-items: center;
   padding: 30px 0;
+  width: 500px;
+  max-width: 100vw;
+  margin: 0 auto;
+}
+
+.wheel-state {
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 20px;
+  color: white;
 }
 
 button {
